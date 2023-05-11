0935c69b5243a71257cb84fdf7b5009b10f4c4df
edwinna <edwinna>
2023-05-11 23:23:03 +0300

    urls to static assets

--- api_integration.py
+++ api_integration.py
@@ -1,37 +1,43 @@
-# #import Flask object from flask package
+#import necessary modules
 from flask import Flask, jsonify
 import requests
 import re
 from bs4 import BeautifulSoup
 
-# #create flask application instance
+
 app = Flask(__name__)
-# #API endpoint
 BASE_URL = "https://cybqa.pesapal.com/pesapalv3"
 
-# #create our decorator(converts a py function into a flask view function)
-# # view function converts the function return value into an HTTP response to be displayed by the HTTP client
+
 @app.route("/", methods=["GET"])
-def api_testing():
+def get_pesapal_iframe_url():
     try:
         #JWT authntication
-        
         #data from the api
         response = requests.get(BASE_URL)
         response.raise_for_status()
+        
         #parse the HTML response using BeautifulSoup
         soup = BeautifulSoup(response.content, "html.parser")
-        #find the container element that wraps the iframes
+        
+        #find all the scripts withing the body
         script_tags = soup.find_all("script")
         
-        
+        #extract the iframe urls from the script tag
         iframe_urls = [script['src'] for script in script_tags if script.get("src")]
         
+        #construct the response to be in json 
+        #response returns URLS of the static assests as the reponse has no <iframe> tags
         response_data = {
             "iframe_urls": iframe_urls
         }
         #response in json
         return jsonify(response_data), 200
+    
+        # Return the HTML doc response as the API response
+        # return Response(response.content, content_type='text/html')
+        
+        
     except requests.exceptions.RequestException as e:
         #handle request errors
         return jsonify({"error": str(e)}), 500
