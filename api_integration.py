26d33f3722274dd40b77db63e5f1ec3e85edc452
edwinna <edwinna>
2023-05-11 23:41:11 +0300

    authentication added

--- api_integration.py
+++ api_integration.py
@@ -1,18 +1,42 @@
 #import necessary modules
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import re
 from bs4 import BeautifulSoup
+from functools import wraps
+import jwt
 
 
 app = Flask(__name__)
+# Change to a secure secret key
+app.config['SECRET_KEY'] = 'your-secret-key'
+
 BASE_URL = "https://cybqa.pesapal.com/pesapalv3"
 
+# JWT authentication decorator
+def jwt_authentication_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({"error": "JWT token is missing"}), 401
 
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            
+            
+        except jwt.ExpiredSignatureError:
+            return jsonify({"error": "JWT token has expired"}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"error": "Invalid JWT token"}), 401
+        
+        return f(*args, **kwargs)
+    return decorated_function
+
+# API route with JWT authentication
 @app.route("/", methods=["GET"])
 def get_pesapal_iframe_url():
     try:
-        #JWT authntication
         #data from the api
         response = requests.get(BASE_URL)
         response.raise_for_status()
@@ -33,7 +57,6 @@ def get_pesapal_iframe_url():
         }
         #response in json
         return jsonify(response_data), 200
-    
         # Return the HTML doc response as the API response
         # return Response(response.content, content_type='text/html')
         
@@ -43,5 +66,3 @@ def get_pesapal_iframe_url():
         return jsonify({"error": str(e)}), 500
     
     
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
