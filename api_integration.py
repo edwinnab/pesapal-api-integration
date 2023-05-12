9ed401d7bb3dc1308619f3205638aed1018cd7e7
edwinna <edwinna>
2023-05-12 10:22:32 +0300

    generate access token

--- api_integration.py
+++ api_integration.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 # Change to a secure secret key
 app.config['SECRET_KEY'] = 'your-secret-key'
 
+
+
 BASE_URL = "https://cybqa.pesapal.com/pesapalv3"
 
 # JWT authentication decorator
@@ -33,36 +35,67 @@ def jwt_authentication_required(f):
     return decorated_function
 
 # API route with JWT authentication
-@app.route("/", methods=["GET"])
-def get_pesapal_iframe_url():
-    try:
-        #data from the api
-        response = requests.get(BASE_URL)
-        response.raise_for_status()
+# @app.route("/", methods=["GET"])
+# def get_pesapal_iframe_url():
+#     try:
+#         #data from the api
+#         response = requests.get(BASE_URL)
+#         response.raise_for_status()
         
-        #parse the HTML response using BeautifulSoup
-        soup = BeautifulSoup(response.content, "html.parser")
+#         #parse the HTML response using BeautifulSoup
+#         soup = BeautifulSoup(response.content, "html.parser")
         
-        # #find all the scripts withing the body
-        script_tags = soup.find_all("script")
-        #check if the script is found
-        if script_tags:
-            #extract the iframe urls from the script tag
-            iframe_urls = [script['src'] for script in script_tags if script.get("src")]
-            #construct the response to be in json 
-            #response returns URLS of the static assests as the reponse has no <iframe> tags
-            response_data = {
-                "iframe_urls": iframe_urls
-            }
-            #response in json
-            return jsonify(response_data), 200
-        else:
-            return jsonify({"error": "Iframe URL not found"}), 500
+#         # #find all the scripts withing the body
+#         script_tags = soup.find_all("script")
+#         #check if the script is found
+#         if script_tags:
+#             #extract the iframe urls from the script tag
+#             iframe_urls = [script['src'] for script in script_tags if script.get("src")]
+#             #construct the response to be in json 
+#             #response returns URLS of the static assests as the reponse has no <iframe> tags
+#             response_data = {
+#                 "iframe_urls": iframe_urls
+#             }
+#             #response in json
+#             return jsonify(response_data), 200
+#         else:
+#             return jsonify({"error": "Iframe URL not found"}), 500
     
-        # Return the HTML doc response as the API response
-        # return Response(response.content, content_type='text/html')        
-    except requests.exceptions.RequestException as e:
-        #handle request errors
-        return jsonify({"error": str(e)}), 500
+#         # Return the HTML doc response as the API response
+#         # return Response(response.content, content_type='text/html')        
+#     except requests.exceptions.RequestException as e:
+#         #handle request errors
+#         return jsonify({"error": str(e)}), 500
+
+# POST Request 
+# Authentication endpoint 
+#generate the access_token 
+#endpoint https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken
+
+api_url = "https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken"
+
+consumer_key = "qkio1BGGYAXTu2JOfm7XSXNruoZsrqEW"
+consumer_secret = "osGQ364R49cXKeOYSpaOnT++rHs="
+
+def generate_access_token():
+    #Request Headers
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    }
+    #Request Payload
+    payload = {
+    "consumer_key": consumer_key,
+    "consumer_secret": consumer_secret
+    }
     
+    #response with the token
+    response = requests.post(api_url, json=payload, headers=headers)
     
+    if response.status_code == 200:
+        access_token = response.json()["token"]
+        print("Access Token", access_token)
+    else:
+        print("Failed to generate access token. Status code:", response.status_code)
+        
+generate_access_token()
